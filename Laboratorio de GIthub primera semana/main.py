528a4dbabc22c98fe3c41ebe5296b13bf90cb471
Yandell20Molina <yandelliphone1@gmail.com>
2026-08-25 13:29:22 -0400

    Add two new grades input fields
    Added two new input fields for additional grades.

--- Laboratorio de GIthub primera semana/main.py
+++ Laboratorio de GIthub primera semana/main.py
@@ -5,6 +5,10 @@ nombre = input("Ingrese el nombre del estudiante: ")
 nota1 = float(input("Ingrese la primera nota: "))
 nota2 = float(input("Ingrese la segunda nota: "))
 nota3 = float(input("Ingrese la tercera nota: "))
+nota4 = float(input("Ingrese la tercera nota: "))
+nota5 = float(input("Ingrese la tercera nota: "))
+
+#en esta parte se añadieron dos notas nuevas que se ouedan ingresar
 
 promedio = (nota1 + nota2 + nota3) / 3
 
