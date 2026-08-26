2a475ec0d4586f917bfb997ad528fd0f090d4ef6
Yandell20Molina <yandelliphone1@gmail.com>
2026-08-26 23:58:41 +0000

    Añade dos notas y actualiza el cálculo del promedio

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ nombre = input("Ingrese el nombre del estudiante: ")
 nota1 = float(input("Ingrese la primera nota: "))
 nota2 = float(input("Ingrese la segunda nota: "))
 nota3 = float(input("Ingrese la tercera nota: "))
+nota4 = float(input("Ingrese la cuarta nota: "))
+nota5 = float(input("Ingrese la quinta nota: "))
+#en esta parte de aqui lo que se hizo fue añadir dos notas mas y se arreglo el input de donde sale el string
 
-promedio = (nota1 + nota2 + nota3) / 3
+promedio = (nota1 + nota2 + nota3 + nota4 + nota5 ) / 5
+#aqui se añdieron dos notas mas a la equacion 
 
 print("\n=== Resultado ===")
 print("Estudiante:", nombre)
